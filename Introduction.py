8b27ff8818bebc7e62abf0106c35e19e76292965
AdityaSurve <surveaditya521@gmail.com>
2023-04-07 19:08:59 +0530

    final

--- Introduction.py
+++ Introduction.py
@@ -1,25 +1,105 @@
-import tensorflow as tf
-print(tf.version)
+# import kerea mnist dataset
+import os
+from keras.datasets import mnist
+from keras.models import model_from_json
 
-string=tf.Variable("this is a string",tf.string)
-number=tf.Variable(324,tf.int16)
-floating=tf.Variable(3.567,tf.float64)
+# load the data
 
-print(string)
-print(number)
-print(floating)
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-rank1_tensor=tf.Variable(["Test","Python","Aditya"],tf.string)
-rank2_tensor=tf.Variable([["Test1","Python","Aditya"],["Test2","Tensorflow","Surve"]],tf.string)
+# reshape the data
 
-print(tf.rank(rank1_tensor)) 
-print(tf.rank(rank2_tensor))
-print(rank1_tensor.shape)
-print(rank2_tensor.shape)
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
 
-tensor_1=tf.ones([2,2,3])
-print(tensor_1)
-tensor_2=tf.reshape(tensor_1,[3,4])
-print(tensor_2)
-tensor_3=tf.reshape(tensor_1,[2,-1])
-print(tensor_3)
+# convert the data to float
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+# normalize the data
+
+x_train =x_train/ 255
+x_test =x_test/ 255
+
+# one hot encoding
+
+from keras.utils import np_utils
+
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+
+# define the model
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+
+model = Sequential()
+
+model.add(Conv2D(32, (3, 3), input_shape=(28, 28, 1), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(10, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# fit the model
+
+model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=10, batch_size=200)
+
+# evaluate the model
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+# save the model
+
+model_structure= model.to_json()
+file=open("model_structure.json","w")
+file.write(model_structure)
+file.close()
+
+model.save_weights("model_weights.h5")
+
+# load the model
+
+file=open("model_structure.json")
+model_structure=file.read()
+model=model_from_json(model_structure)
+model.load_weights("model_weights.h5")
+
+# predict
+
+import numpy as np
+
+class_labels=[
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    
+]
+list_of_images=np.expand_dims(x_test,axis=0)
+results=model.predict(list_of_images)
+single_result=results[0]
+most_likely_class_index=int(np.argmax(single_result))
+class_likelihood=single_result[most_likely_class_index]
+class_label=class_labels[most_likely_class_index]
+print("Image = {}-Likelihood: {:2f}".format(class_label, class_likelihood))
--- Linear.py
+++ Linear.py
@@ -9,3 +9,43 @@ plt.plot(x, y, 'ro')
 plt.axis([0, 6, 0, 20])
 plt.plot(np.unique(x), np.poly1d(np.polyfit(x, y, 1))(np.unique(x)))
 plt.show()
+
+#Image Recognition
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv2.imread('datasets/Pants.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(gray, (5, 5), 0)
+thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+for c in cnts:
+    area = cv2.contourArea(c)
+    if area > 1000:
+        x, y, w, h = cv2.boundingRect(c)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+plt.imshow(img)
+plt.show()
+
+# Face Recognition
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv2.imread('datasets/faces.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(gray, (5, 5), 0)
+thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+for c in cnts:
+    area = cv2.contourArea(c)
+    if area > 1000:
+        x, y, w, h = cv2.boundingRect(c)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 20)
+plt.imshow(img)
+plt.show()
